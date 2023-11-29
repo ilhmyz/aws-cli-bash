c88d25bc1e07f954babf8c14dd31d28e7bbb14c7
ilhmyz <ilhamyuz16@gmail.com>
2023-11-29 19:43:50 +0700

    update script

--- install_aws.sh
+++ install_aws.sh
@@ -13,8 +13,8 @@ if [[ "$architecture" == "x86_64" ]]; then
     sleep 3
     # Install AWS CLI for 64-bit
     curl "https://awscli.amazonaws.com/awscli-exe-linux-$architecture.zip" -o "/tmp/awscliv2.zip"
-    unzip /tmp/awscliv2.zip
-    sudo ./tmp/aws/install
+    unzip /tmp/awscliv2.zip -d /tmp
+    sudo /tmp/aws/install
     echo "remove aws install dir and zip"
     rm -rf /tmp/aws*
 elif [[ "$architecture" == "aarch64" ]]; then
@@ -22,8 +22,8 @@ elif [[ "$architecture" == "aarch64" ]]; then
     sleep 3
     # Install AWS CLI for ARM
     curl "https://awscli.amazonaws.com/awscli-exe-linux-$architecture.zip" -o "/tmp/awscliv2.zip"
-    unzip /tmp/awscliv2.zip
-    sudo ./tmp/aws/install
+    unzip /tmp/awscliv2.zip -d /tmp
+    sudo /tmp/aws/install
     echo "remove aws install dir and zip"
     rm -rf /tmp/aws*
 else
